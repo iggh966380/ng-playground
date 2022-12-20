32d4d32ecd84b5e60fd1d3cdfb625749606146c9
Jason Chang <jason@aiii.ai>
2022-12-20 14:11:41 +0800

    新增di

--- src/app/DI/di.module.ts
+++ src/app/DI/di.module.ts
@@ -1,3 +1,6 @@
+import { BetterLogger } from './service/betterLogger.service';
+import { Logger } from './service/logger.service';
+import { HeroService } from './service/hero.service';
 import { HeroComponent } from './service/hero.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -5,5 +8,6 @@ import { CommonModule } from '@angular/common';
 @NgModule({
   declarations: [HeroComponent],
   imports: [CommonModule],
+  providers: [{ provide: HeroService }],
 })
 export class DIModule {}
--- src/app/DI/service/betterLogger.service.ts
+++ src/app/DI/service/betterLogger.service.ts
@@ -1,3 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HeroService } from './hero.service';
 import { Logger } from './logger.service';
 
-export class BetterLogger extends Logger {}
+@Injectable()
+export class BetterLogger extends Logger {
+  constructor(private userService: HeroService) {
+    super();
+  }
+
+  override log(message: any) {
+    const hero = this.userService.getHero();
+    super.log(`message to ${hero.name}`);
+  }
+}
